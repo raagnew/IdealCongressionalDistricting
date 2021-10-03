44722e75cb99a819d8d0d9dc624c3a9dec308f5a
raagnew <35945145+raagnew@users.noreply.github.com>
2021-10-03 11:12:40 -0400

    Add files via upload

---
+++ MI_Extract.R
@@ -0,0 +1,26 @@
+
+# -----------------------------
+# Specify path to MI header file migeo2020.pl downloaded from https://www2.census.gov/programs-surveys/decennial/2020/data/01-Redistricting_File--PL_94-171/
+# -----------------------------
+header_file_path <- "c:/CONGRESSIONAL_DISTRICTING/migeo2020.pl"
+
+# -----------------------------
+# Import the data
+# -----------------------------
+header <- read.delim(header_file_path, header=FALSE, colClasses="character", sep="|")
+colnames(header) <- c("FILEID", "STUSAB", "SUMLEV", "GEOVAR", "GEOCOMP", "CHARITER", "CIFSN", "LOGRECNO", "GEOID", 
+                      "GEOCODE", "REGION", "DIVISION", "STATE", "STATENS", "COUNTY", "COUNTYCC", "COUNTYNS", "COUSUB",
+                      "COUSUBCC", "COUSUBNS", "SUBMCD", "SUBMCDCC", "SUBMCDNS", "ESTATE", "ESTATECC", "ESTATENS", 
+                      "CONCIT", "CONCITCC", "CONCITNS", "PLACE", "PLACECC", "PLACENS", "TRACT", "BLKGRP", "BLOCK", 
+                      "AIANHH", "AIHHTLI", "AIANHHFP", "AIANHHCC", "AIANHHNS", "AITS", "AITSFP", "AITSCC", "AITSNS",
+                      "TTRACT", "TBLKGRP", "ANRC", "ANRCCC", "ANRCNS", "CBSA", "MEMI", "CSA", "METDIV", "NECTA",
+                      "NMEMI", "CNECTA", "NECTADIV", "CBSAPCI", "NECTAPCI", "UA", "UATYPE", "UR", "CD116", "CD118",
+                      "CD119", "CD120", "CD121", "SLDU18", "SLDU22", "SLDU24", "SLDU26", "SLDU28", "SLDL18", "SLDL22",
+                      "SLDL24", "SLDL26", "SLDL28", "VTD", "VTDI", "ZCTA", "SDELM", "SDSEC", "SDUNI", "PUMA", "AREALAND",
+                      "AREAWATR", "BASENAME", "NAME", "FUNCSTAT", "GCUNI", "POP100", "HU100", "INTPTLAT", "INTPTLON", 
+                      "LSADC", "PARTFLAG", "UGA")
+header <- header[,c(3,10,33,34,91,93,94)]
+x <- header[header[,1]=="150",c(2,3,4,5,6,7)]
+write.table(x,file="c:/CONGRESSIONAL_DISTRICTING/MI_Blkgrps.txt",sep=",",row.names=FALSE,col.names=TRUE)
+
+
